54339f25d47e76f10927b72b47a9f05068c67245
kimyundung <kimyundung@naver.com>
2021-07-28 17:49:40 +0800

    注解

--- jihe/src/com/stone/testAnnotation/Man.java
+++ jihe/src/com/stone/testAnnotation/Man.java
@@ -4,6 +4,12 @@ package com.stone.testAnnotation;
 @ManType(value = "超人")
 //@ManTypes({@ManType(value = "职工"), @ManType(value = "超人")}) //java8之前处理多个注解的方式
 public class Man {
+
+    @Deprecated // 表示该方法已经过时, 不建议使用
+    public void show(){
+        System.out.println("这个方法马上要过时了!");
+    }
+
     public static void main(String[] args) {
         int ia = 97;
         char c1 = (@ManType char) ia;
---
+++ jihe/src/com/stone/testAnnotation/ManTest.java
@@ -0,0 +1,9 @@
+package com.stone.testAnnotation;
+
+public class ManTest {
+
+    public static void main(String[] args) {
+        Man man = new Man();
+        man.show();
+    }
+}
